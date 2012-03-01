08a63487e2d8762b1b9684f09ea26f911240bcbf
tomnatt <github@tomnatt.com>
2012-03-01 08:52:55 +0000

    tabs to spaces

--- roller.php
+++ roller.php
@@ -4,18 +4,18 @@ $rolled = 4;
 $kept = 2;
 
 function rollD10() {
-	$result = rand(1, 10);
-	if ($result == 10) {
-		$result = $result + rollD10();
-	}
-	return $result;
+    $result = rand(1, 10);
+    if ($result == 10) {
+        $result = $result + rollD10();
+    }
+    return $result;
 }
 
 // generate every result
 $results = array();
 
 for ($i = 0; $i < $rolled; $i++) {
-	$results[] = rollD10();
+    $results[] = rollD10();
 }
 
 // sort them
@@ -24,7 +24,7 @@ rsort($results, SORT_NUMERIC);
 // sum the highest kept
 $total = 0;
 for ($i = 0; $i < $kept; $i++) {
-	$total = $total + $results[$i];
+    $total = $total + $results[$i];
 } 
 print_r($results);
 echo $total;
