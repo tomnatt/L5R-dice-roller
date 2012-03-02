c0e7286451b568f81048b1f68d8244ed1fb05c88
tomnatt <github@tomnatt.com>
2012-03-02 11:36:24 +0000

    progress

--- index.php
+++ index.php
@@ -8,9 +8,70 @@ require_once("l5r_dice_roller.php");
 $roll = 7;
 $keep = 4;
 
-$rolling = new l5r_dice_roller($roll, $keep);
+$rolling = new l5r_dice_roller($roll, $keep, 10);
 
 $rolling->roll();
-echo $rolling->result();
+
+//print_r($rolling->results());
+echo json_encode($rolling->results());
 
 ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>L5R Dice roller</title>
+
+  <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="js/excanvas.js"></script><![endif]-->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
+    <!--<script type="text/javascript" src="js/plugins/jqplot.dateAxisRenderer.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.barRenderer.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.categoryAxisRenderer.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.cursor.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.highlighter.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.dragable.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jqplot.trendline.min.js"></script>-->
+    <link rel="stylesheet" type="text/css" hrf="css/jquery.jqplot.min.css" />
+   
+</head>
+<body>
+
+<div id="results" style="height:400px; width:800px;"></div>
+
+<p>Average result: <?php echo $rolling->averageResult(); ?></p>
+
+<script>
+$(document).ready(function () {
+
+    $.jqplot.config.enablePlugins = true;
+ 
+    var s1 = $.parseJSON(<?php echo json_encode(json_encode($rolling->results())); ?>);
+    
+    console.log(s1);
+    console.log($.isArray(s1));
+ 
+    // include a little space around the mix / max for a more pleasing graph
+    plot1 = $.jqplot('results',[s1],{
+        title: 'Results',
+        axes: {
+            xaxis: {
+              min: <?php echo $rolling->lowestRolled(); ?> - 5,
+              max: <?php echo $rolling->highestRolled(); ?> + 5,  
+                
+            },
+            yaxis: {
+                min: 0,
+                max: <?php echo $rolling->mostRolled(); ?> + 2
+            }
+        },
+        cursor: {
+            show: true
+        }
+    });
+});
+</script>
+
+</body>
+</html>
--- l5r_dice_roller.php
+++ l5r_dice_roller.php
@@ -2,12 +2,13 @@
 
 class l5r_dice_roller {
     
-    protected $roll, $keep, $rounds, $results;
+    protected $roll, $keep, $rounds, $rawResults, $results, $mostRolled, $highestRolled, $lowestRolled;
     
     public function __construct($roll, $keep, $rounds = 1000) {  
         $this->roll = $roll;
         $this->keep = $keep;
         $this->rounds = $rounds;
+        $this->rawResults = array();
         $this->results = array();
     }
     
@@ -47,15 +48,53 @@ class l5r_dice_roller {
         
         // roll your xKy dice $rounds times so we can get an average 
         for ($i = 0; $i < $this->rounds; $i++) {
-            $this->results[] = $this->singleResult();
+            $this->rawResults[] = $this->singleResult();
+        }
+        
+        // sort for easier use
+        sort($this->rawResults, SORT_NUMERIC);
+        
+        $this->lowestRolled = $this->rawResults[0];
+        $this->highestRolled = $this->rawResults[count($this->rawResults) - 1];
+   
+        // create multidimensional array of result => occurances, removing dupes
+        $used = array();
+        foreach ($this->rawResults as $r) {
+            if (!in_array($r, $used)) {
+            
+                // store the greatest instances of a result rolled
+                $timesRolled = count(array_keys($this->rawResults, $r));
+                if ($timesRolled > $this->mostRolled) {
+                    $this->mostRolled = $timesRolled;
+                }
+            
+                $this->results[] = array("$r", $timesRolled);
+                $used[] = $r;
+            }
         }
         
     }
     
-    public function result() {
+    public function mostRolled() {
+        return $this->mostRolled;        
+    }
+    
+    public function highestRolled() {
+        return $this->highestRolled;        
+    }
+    
+    public function lowestRolled() {
+        return $this->lowestRolled;        
+    }
+    
+    public function results() {
+        return $this->results;        
+    }
+    
+    public function averageResult() {
         
         // take the average of all the result sets, round to nearest whole number
-        return round(array_sum($this->results) / count($this->results));
+        return round(array_sum($this->rawResults) / count($this->rawResults));
         
     }
     
